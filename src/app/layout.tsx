06d9542527f02f9ff5769bd926c3afbb1885140c
Aizhee <atjamilano@slsu.edu.ph>
2025-08-21 13:16:31 +0800

    update favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>
         <script dangerouslySetInnerHTML={{ __html: `(function(){try{var t=localStorage.getItem('theme');var d=localStorage.getItem('darkMode');var prefers=window.matchMedia&&window.matchMedia('(prefers-color-scheme: dark)').matches;var dark = t==='dark'?true:t==='light'?false:d!==null?JSON.parse(d):prefers; if(dark){document.documentElement.classList.add('dark');document.documentElement.setAttribute('data-theme','dark');document.documentElement.style.colorScheme='dark';}else{document.documentElement.classList.remove('dark');document.documentElement.setAttribute('data-theme','light');document.documentElement.style.colorScheme='light';}}catch(e){}})();` }} />
       </head>
-      <body>
+    <link rel="icon" href="https://avatars.githubusercontent.com/u/108914551" />
         <NextThemeProvider>{children}</NextThemeProvider>
       </body>
     </html>
